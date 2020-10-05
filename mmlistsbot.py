49ce6c3c3226058eb7376bbccd9f194a84b51651
U-C-2963\Cassagnes Andres <Cassagnes Andres@C-2963.invap.com.ar>
2020-10-05 19:13:46 -0300

    first commit

---
+++ mmlistsbot.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+A bot that allow you to query Miralos Morir movie lists
+"""
+
+import logging
+import os
+import random
+import sys
+import json
+import requests
+from datetime import datetime, timedelta
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ParseMode
+
+b_key = "1306345595:AAHyodPBoZVLU_pH-ZThoxu0-aTjZv5oFwM"
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+# Getting mode, so we could define run function for local and Heroku setup
+#mode = os.getenv("MODE")
+mode = "dev"
+TOKEN = os.getenv("TOKEN")
+if mode == "dev":
+    TOKEN = b_key
+    def run(updater):
+        updater.start_polling()
+elif mode == "prod":
+    def run(updater):
+        PORT =  int(os.environ.get("PORT", "8443"))
+        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
+        # Code from https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks#heroku
+        updater.start_webhook(listen="0.0.0.0",
+                              port=PORT,
+                              url_path=TOKEN)
+        updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
+else:
+    logger.error("No MODE  specified!")
+    sys.exit(1)
+
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    start_msg = "Hola, soy un bot calorista, al que podés consultarle por las películas nombradas en cada "
+    start_msg += "edición de Míralos Morir \n"
+    start_msg += "Para saber como interactuar conmigo, escribí /aiuuda y obtendrás una lista de comandos."
+    update.message.reply_text(start_msg)
+
+def show_help(update, context):
+    """Send a message when the command /start is issued."""
+    help_msg = "Usá los siguientes comandos para comunicarte conmigo:\n"
+    help_msg += "/listar_todas para ver todas las listas disponibles \n"
+    help_msg += "/MM 'nro' para ver una lista en particular con sus películas, por ejemplo: /MM 15"
+    update.message.reply_text(help_msg)
+
+def list_all(update, context):
+    lists_url = "http://miralosmorserver.pythonanywhere.com/api/movielists-mm"
+    lists_req = requests.get(lists_url)
+    lists_dict = lists_req.json()
+    lists_msg = ''
+    list_count = 0
+    for l in lists_dict:
+        lists_msg += '<b>' + l['name'] + '</b>' + '\n'
+        lists_msg += l['description'] + '\n'
+        #lists_msg += l['link'] +'\n \n'    
+        lists_msg += '\n'
+        list_count += 1
+        if list_count > 4:
+            update.message.reply_text(lists_msg,
+                  parse_mode=ParseMode.HTML) 
+            lists_msg = ''
+            list_count = 0
+    if len(lists_msg) > 0:
+        update.message.reply_text(lists_msg,
+                  parse_mode=ParseMode.HTML)
+
+def get_mm(update, context):
+    """ Get specific list """
+    try:
+        # args[0] should contain the time for the timer in seconds
+        lists_url = "http://miralosmorserver.pythonanywhere.com/api/movielists-mm"
+        lists_req = requests.get(lists_url)
+        lists_dict = lists_req.json()
+        list_n = int(context.args[0])
+        if list_n < 0 or list_n > len(lists_dict):
+            update.message.reply_text('Poné un número válido hermano. Entre 1 y ' + str(len(lists_dict)))
+            return
+
+        # Get the list
+        mm_url = "http://miralosmorserver.pythonanywhere.com/api/movieliststag/"
+        mm_url += "MiralosMorir" + str(list_n)
+        mm_req = requests.get(mm_url)
+        mm_dict = mm_req.json()[0]
+
+        mm_msg = '<b>' + mm_dict['name'] + '</b>' + '  ' + mm_dict['description'] + '\n'
+        mm_msg += mm_dict['link']
+
+        update.message.reply_text(text=mm_msg, 
+                  parse_mode=ParseMode.HTML)
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /MM <numero>')
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("aiuuda", show_help))
+    dp.add_handler(CommandHandler("listar_todas", list_all))
+    dp.add_handler(CommandHandler("MM", get_mm,
+                                  pass_args=True,
+                                  pass_chat_data=True))
+
+    # Start the Bot
+    run(updater)
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
