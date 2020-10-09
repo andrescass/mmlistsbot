9d1f0afdaf5f6e21ab139aaa4418f1d287c77b6b
U-C-2963\Cassagnes Andres <Cassagnes Andres@C-2963.invap.com.ar>
2020-10-09 11:23:31 -0300

    link 3

--- mmlistsbot.py
+++ mmlistsbot.py
@@ -182,13 +182,19 @@ def get_name(update, context):
         movie_count = 0
         current_movie = ''
         for movie in movie_dict:
+
             current_movie = '<b>' + movie['movie_name'] + '</b> (' + movie['movie_year'] + ')\n'
             current_movie += 'Directed by ' + movie['movie_director'] + '\n'
             if len(movie['movie_detail']) > 0:
                 current_movie += movie['movie_detail'] + '\n' 
-            for l in movie['movie_lists'].split(','):
-                current_movie += l + '\n'
-
+            
+            list_links = movie['movie_lists_links'].split(',')
+            list_names = movie['movie_lists'].split(',')
+            #for l in movie['movie_lists'].split(','):
+            for i in range(0, len(list_names)):
+                current_movie += "<a href=\""+ list_links[i] +" \">" + list_names[i] +"</a> \n"
+                #current_movie += l + '\n'
+                
             if (len(current_movie) + len(movie_msg)) < 4096:
                 movie_msg += current_movie
                 movie_count += 1
